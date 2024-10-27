f665e46a94122d8cedc5ed54dc0f6f7d014b55c4
Connor <cdubyamason@gmail.com>
2024-10-27 15:50:48 -0400

    incremented version numbers so hopefully publish works again

---
+++ powershell-utility-scripts/Shared/Compare-Strings/Compare-Strings.Tests.ps1
@@ -0,0 +1,25 @@
+Describe "Compare-Strings" {
+    Context "Comparing two identical strings" {
+        It "Should return true" {
+            Compare-Strings "hello" "hello" | Should -Be $true
+        }
+    }
+
+    Context "Comparing two different strings" {
+        It "Should return false" {
+            Compare-Strings "hello" "world" | Should -Be $false
+        }
+    }
+
+    Context "Comparing case-sensitive strings" {
+        It "Should return false for different cases" {
+            Compare-Strings "hello" "HELLO" | Should -Be $false
+        }
+    }
+
+    Context "Comparing empty strings" {
+        It "Should return true" {
+            Compare-Strings "" "" | Should -Be $true 
+        }
+    }
+}
