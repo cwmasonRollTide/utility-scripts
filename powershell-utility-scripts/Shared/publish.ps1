ca167c86fc38bdac7aed18f96417b380716bc170
Connor <cdubyamason@gmail.com>
2024-10-26 20:04:20 -0400

    ah okay during github action exection the directory struction is fucked. so using the MyInvocation.Path... etc.

--- powershell-utility-scripts/Shared/publish.ps1
+++ powershell-utility-scripts/Shared/publish.ps1
@@ -1,4 +1,5 @@
-$modules = @("Open-File", "Save-File", "Get-LargeTextInput", "Compare-Strings", "Search-ExcelContent")
+$modules = @("Open-File", "Save-File", "Get-LargeTextInput", "Compare-Strings", "Search-ExcelContent") 
+$scriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 foreach ($module in $modules) {
-    Publish-Module -Path "./$module" -NuGetApiKey $env:NUGET_KEY -Force
+    Publish-Module -Path "$scriptDir/$module" -NuGetApiKey $env:NUGET_KEY -Force
 }
