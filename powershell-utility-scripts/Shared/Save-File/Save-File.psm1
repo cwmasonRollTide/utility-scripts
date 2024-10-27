0f01cbc95a46d6f9a9e70ac999270e47a19578d3
Connor <cdubyamason@gmail.com>
2024-10-27 17:46:00 -0400

    manually updating version number for now

--- powershell-utility-scripts/Shared/Save-File/Save-File.psm1
+++ powershell-utility-scripts/Shared/Save-File/Save-File.psm1
@@ -1,6 +1,6 @@
 @{
     RootModule = 'Save-File.psm1'
-    ModuleVersion = '1.0'
+    ModuleVersion = '1.0.2'
     Author = 'Connor Mason'
     Description = 'Easy to use module for saving data to a file of the following types: .xlsx ,.xls ,.csv ,.json ,.xml ,.txt.,All Files'
 }
