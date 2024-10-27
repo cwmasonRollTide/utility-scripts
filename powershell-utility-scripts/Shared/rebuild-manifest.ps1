7e7997ac69873b6f8d057bf37d043e9844e48ee2
Connor <cdubyamason@gmail.com>
2024-10-27 16:57:05 -0400

    stop freaking out git

--- powershell-utility-scripts/Shared/rebuild-manifest.ps1
+++ powershell-utility-scripts/Shared/rebuild-manifest.ps1
@@ -1,15 +1,3 @@
 foreach ($file in (Get-ChildItem -Path "./Shared" -Filter "*.psd1" -Recurse)) {
     New-ModuleManifest -Path $file.FullName
 }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> 3ca2d7a2a5152cb9a8fbe288bc0dbabb6be5c47c
-
-
->>>>>>> 863ccec85f4b03940c0cfe8027a06c70fa3cddf0
